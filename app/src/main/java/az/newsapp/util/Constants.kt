b6ee1486eee325ae382cca2719c0cd1d8d50d6e2
Omar Adel <omarzero1015@gmail.com>
2020-12-21 03:28:41 +0200

    changing api_key due to limit requests reached

--- app/src/main/java/az/newsapp/util/Constants.kt
+++ app/src/main/java/az/newsapp/util/Constants.kt
@@ -2,7 +2,7 @@ package az.newsapp.util
 
 class Constants {
     companion object {
-        const val API_KEY = "9985b87ec1f2433ea5b26576e4ed442e"
+        const val API_KEY = "1078058ec4144ea9b30741c553fd4128"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val COUNTRY_CODE = "us"
